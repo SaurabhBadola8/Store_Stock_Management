6171f9d3fffa9dab45bbb16d0a93cff78d2787bb
SAURABH BADOLA <shourya003b@gmail.com>
2024-06-26 15:31:21 +0530

    stock _check_2nd Frame

---
+++ stock2.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from pymysql import *
+from tkinter import messagebox as msg
+class cdata(Frame):
+    def __init__(self,master):
+        super().__init__(master)
+       
+        self.ta=Text(self)
+        self.ta.grid(row=0,column=0)
+       
+        con=connect(db='badola',user='root',password='system',host='localhost')
+        cur=con.cursor()
+        cur.execute("select * from stock")
+        data=cur.fetchall()
+        txt=""
+        for xdata in data:
+            for x in xdata:
+                txt+=str(x)+"\t"
+            txt+="\n"
+        self.ta.insert(END,txt)
+        self.pack()
